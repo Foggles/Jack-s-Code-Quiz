88f59659a63f9e00a4ea555f193f3d9787bd6fa1
Foggles <jackfogarty2105@gmail.com>
2020-12-05 16:26:14 +0800

    showQuestions function now successfully displays question and answer choices

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// Assigning the the name and choices from questions object to const variables
-let questionElement = questions.question;
-let answerButtonsElement = questions.choices;
-
-debugger;
-
 // Grabbing elements from index.html
+const questionElement = document.getElementById("question");
+const answerElement = document.getElementById("answer-buttons");
 const startButton = document.getElementById("start-button");
 const questionContainerElement = document.getElementById("question-container");
 
 // Variables to allow for questions to be randomised
-let shuffledQuestions;
 let currentQuestionIndex;
 
 // Let variables for the timer to function
@@ -18,75 +13,42 @@ let timeRemaining = 75;
 
 // Function to start the timer
 function startTimer() {
-    setInterval(function () {
-        timeRemaining = timeRemaining - 1;
-        timeDisplay.textContent = "Time Remaining: " + timeRemaining;
-    }, 1000);
+  setInterval(function () {
+    timeRemaining = timeRemaining - 1;
+    timeDisplay.textContent = "Time Remaining: " + timeRemaining;
+  }, 1000);
 };
 
 // Function to execute once start button has been clicked
 function startQuiz() {
-    console.log("started");
-    startButton.classList.add("hide");
-    shuffledQuestions.questionElement.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion();
-};
-
-// Function to display next question once the previous question has been answered
-function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  console.log("started");
+  startButton.classList.add("hide");
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  showQuestion(questions[currentQuestionIndex]);
 };
 
 // Function to display question
-function showQuestion(questionElement) {
-    questionElement.innerText = questions.question;
+function showQuestion(questionObj) {
+  questionElement.innerText = questionObj.question;
+//   answerElement.innerText = questionObj.choices;
+
+  for (let index = 0; index < questionObj.choices.length; index++) {
+      let answerButton = document.createElement("button");
+      answerButton.innerText = questionObj.choices[index].answer;
+      answerButton.classList.add("btn");
+      answerButton.addEventListener("click", selectAnswer);
+      answerElement.appendChild(answerButton);
+  };
+  // DISPLAY ANSWERS
+  // ATTACH EVENT TO ANSWERS
+  // EVENT SHOULD INCREASE currentQuestionIndex
 };
 
-function selectAnswer() {
-
-};
+function selectAnswer() {};
 
 // When the start quiz button is clicked (add event listerning to the start button)
 // & start the timer (set Interval)
 startButton.addEventListener("click", startTimer);
-
 // Update the DOM with the first question and display the choices assoc with the question.
 startButton.addEventListener("click", startQuiz);
-
-
-
-
-
-// If user selected the correct answer
-// Load the next question + the assoc choices to the DOM
-// Tell the user they selected the correct answer
-
-
-// If user selected the wrong answer
-// Decrease the timer by 10 seconds
-// Load the next question + the assoc choices to the DOM
-// Tell the user that they selected the wong answer
-
-
-// Also need a way to check for game over
-// Game over when:
-// 1. Time becomes 0 or <0
-// 2. User finished all the questions available
-
-
-// What to do once game is finished
-// 1. Display result
-// Clear the DOM
-// Ask for initial/username
-// Show the final score
-
-
-// 2. Prompt the user to start a new game
-// Store the result in local storage
-
-
-
-
-
