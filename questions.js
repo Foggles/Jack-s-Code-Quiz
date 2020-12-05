ddecfad8a3ec6a5d4445546e56f1e4314ab25f74
Foggles <jackfogarty2105@gmail.com>
2020-12-05 15:31:30 +0800

    Pushing current work for debugging purposes

--- questions.js
+++ questions.js
@@ -1,138 +1,136 @@
 // Store questions as objects within objects:
-// 1. Name
+// 1. Question
 // 2. Choices
 // 3. Answer
 
 const questions = [
     // Object for first question
     { 
-        name: "Commonly used data types DO NOT include: ",
+        question: "Commonly used data types DO NOT include: ",
         choices: [
             {
-                name: "1. Strings",
-                isAnswer = false,
+                answer: "1. Strings",
+                isAnswer:false,
             },
 
             {
-                name: "2. Booleans",
-                isAnswer = false,
+                answer: "2. Booleans",
+                isAnswer:false,
             },
 
             {
-                name: "3. Alerts",
-                isAnswer = true,
+                answer: "3. Alerts",
+                isAnswer:true,
             },
 
             {
-                name: "4. Numbers",
-                isAnswer = false,
+                answer: "4. Numbers",
+                isAnswer:false,
             },
         ],
     },
 
     // Object for second question
     { 
-        name: "The condition in an if / else statement is enclosed within ____.",
+        question: "The condition in an if / else statement is enclosed within ____.",
         choices: [
             {
-                name: "1. Quotes",
-                isAnswer = false,
+                answer: "1. Quotes",
+                isAnswer:false,
             },
 
             {
-                name: "2. Curly Brackets",
-                isAnswer = false,
+                answer: "2. Curly Brackets",
+                isAnswer:false,
             },
 
             {
-                name: "3. Parentheses",
-                isAnswer = true,
+                answer: "3. Parentheses",
+                isAnswer:true,
             },
 
             {
-                name: "4. Square Brackets",
-                isAnswer = false,
+                answer: "4. Square Brackets",
+                isAnswer:false,
             },
         ],
     },
 
     // Object for third question
     { 
-        name: "Arrays in JavaScript can be used to store ____.",
+        question: "Arrays in JavaScript can be used to store ____.",
         choices: [
             {
-                name: "1. Numbers & Strings",
-                isAnswer = false,
+                answer: "1. Numbers & Strings",
+                isAnswer:false,
             },
 
             {
-                name: "2. Other Arrays",
-                isAnswer = false,
+                answer: "2. Other Arrays",
+                isAnswer:false,
             },
 
             {
-                name: "3. Booleans",
-                isAnswer = false,
+                answer: "3. Booleans",
+                isAnswer:false,
             },
 
             {
-                name: "4. All of the Above",
-                isAnswer = true,
+                answer: "4. All of the Above",
+                isAnswer:true,
             },
         ],
     },
 
     // Object for fourth question
     { 
-        name: "String values must be enclosed within ____ when being assigned to variables.",
+        question: "String values must be enclosed within ____ when being assigned to variables.",
         choices: [
             {
-                name: "1. Commas",
-                isAnswer = false,
+                answer: "1. Commas",
+                isAnswer:false,
             },
 
             {
-                name: "2. Curly Brackets",
-                isAnswer = false,
+                answer: "2. Curly Brackets",
+                isAnswer:false,
             },
 
             {
-                name: "3. Quotes",
-                isAnswer = true,
+                answer: "3. Quotes",
+                isAnswer:true,
             },
 
             {
-                name: "4. Parentheses",
-                isAnswer = false,
+                answer: "4. Parentheses",
+                isAnswer:false,
             },
         ],
     },
 
     // Object for fifth question
      { 
-        name: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: [
             {
-                name: "1. JavaScript",
-                isAnswer = false,
+                answer: "1. JavaScript",
+                isAnswer:false,
             },
 
             {
-                name: "2. Terminal / Bash",
-                isAnswer = false,
+                answer: "2. Terminal / Bash",
+                isAnswer:false,
             },
 
             {
-                name: "3. For Loops",
-                isAnswer = false,
+                answer: "3. For Loops",
+                isAnswer:false,
             },
 
             {
-                name: "4. console.log",
-                isAnswer = true,
+                answer: "4. console.log",
+                isAnswer:true,
             },
         ],
     },
 ];
-
-export {questions};
--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-import {questions} from "./questions.js";
+// Assigning the the name and choices from questions object to const variables
+let questionElement = questions.question;
+let answerButtonsElement = questions.choices;
 
-questionsList = questions;
+debugger;
 
+// Grabbing elements from index.html
 const startButton = document.getElementById("start-button");
 const questionContainerElement = document.getElementById("question-container");
 
-const shuffledQuestions;
-const currentQuestionIndex;
+// Variables to allow for questions to be randomised
+let shuffledQuestions;
+let currentQuestionIndex;
 
+// Let variables for the timer to function
 let timeDisplay = document.getElementById("time-display");
 let timeRemaining = 75;
 
-
+// Function to start the timer
 function startTimer() {
     setInterval(function () {
         timeRemaining = timeRemaining - 1;
@@ -19,17 +24,24 @@ function startTimer() {
     }, 1000);
 };
 
+// Function to execute once start button has been clicked
 function startQuiz() {
     console.log("started");
     startButton.classList.add("hide");
-    shuffledQuestions.questionsList.sort(() => Math.random() - .5);
+    shuffledQuestions.questionElement.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove("hide");
     setNextQuestion();
 };
 
+// Function to display next question once the previous question has been answered
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
 
+// Function to display question
+function showQuestion(questionElement) {
+    questionElement.innerText = questions.question;
 };
 
 function selectAnswer() {
@@ -73,4 +85,8 @@ startButton.addEventListener("click", startQuiz);
 
 // 2. Prompt the user to start a new game
 // Store the result in local storage
+
+
+
+
 
